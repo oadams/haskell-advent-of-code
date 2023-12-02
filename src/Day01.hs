a92a578b88808340ed9887657675f0f6db4d64d3
Oliver Adams <oliver.adams@gmail.com>
2023-12-02 15:06:01 +1100

    day 1 part 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,10 +1,26 @@
 module Day01 where
 
 import Paths_aoc2023 (getDataFileName)
+import Data.Char (isDigit)
+
+lineNum :: String -> Integer
+lineNum line = read $ [firstDigit line] <> [secondDigit line]
+
+firstDigit :: String -> Char
+firstDigit "" = error "no first char"
+firstDigit (x:xs)
+   | isDigit x = x
+   | otherwise = firstDigit xs
+
+secondDigit :: String -> Char
+secondDigit = firstDigit . reverse
+
+solution :: [String] -> Integer
+solution ls = foldr (+) 0 (map lineNum ls)
 
 day01 :: IO ()
 day01 = do
   inputLines <- lines <$> (getDataFileName "day01-input.txt" >>= readFile)
   putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
+  putStrLn $ show $ solution inputLines
   putStrLn "TODO: implement Day 01"
