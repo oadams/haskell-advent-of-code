27c48a5fdfc8d797c902e4cd7a0f6bdc7071c183
Oliver Adams <oliver.adams@gmail.com>
2023-12-08 21:07:01 +1100

    Finished Day 2.

--- src/Day02.hs
+++ src/Day02.hs
@@ -31,12 +31,19 @@ isValidGameDraw game = (green game) <= 13 && (red game) <= 12 && (blue game) <= 
 isValidGame :: [GameDraw] -> Bool
 isValidGame games = all isValidGameDraw games 
 
+minDraw :: [GameDraw] -> GameDraw
+minDraw [] = GameDraw {green=0, red=0, blue=0}
+minDraw [x] = x
+minDraw (x:xs) = let m = minDraw xs in GameDraw {green = max (green m) (green x), red = max (red m) (red x), blue = max (blue m) (blue x)}
+
+power :: [GameDraw] -> Integer
+power game = let m = minDraw game in (green m) * (red m) * (blue m)
+  
 day02 :: IO ()
 day02 = do
   inputLines <- lines <$> (getDataFileName "day02-input.txt" >>= readFile)
   -- putStrLn $ show $ map parseGames inputLines
-  let games = zip [1..100] (map parseGames inputLines)
-  let validGames = filter (isValidGame . snd) games
+  let games = (map parseGames inputLines)
   -- Sum over the first element of the tuples in validGames
-  let result = sum $ map fst validGames 
+  let result = sum $ map power games 
   putStrLn $ show result
