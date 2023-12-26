cd05298a05a2a778a457a7c0c480796cb1cbddac
Oliver Adams <oliver.adams@gmail.com>
2023-12-26 10:14:53 +1000

    Day 09 part 1.

--- src/Day09.hs
+++ src/Day09.hs
@@ -2,9 +2,30 @@ module Day09 where
 
 import Paths_aoc2023 (getDataFileName)
 
+parseSeq :: String -> [Integer]
+parseSeq line = map read (words line)
+
+diffSeq :: [Integer] -> [Integer]
+diffSeq seq = zipWith (-) (tail seq) seq
+
+diffStack :: [[Integer]] -> [[Integer]]
+diffStack (layer:layers) 
+    | null (diffSeq layer) = layer:layers
+    | all (== 0) (diffSeq layer) = layer:layers
+    | otherwise = diffStack ((diffSeq layer):layer:layers)
+diffStack [] = []
+
+nextVal :: [Integer] -> Integer
+nextVal history = foldr (\x y -> last x + y) 0 stack
+  where
+    stack = diffStack [history]
+
 day09 :: IO ()
 day09 = do
-    inputLines <- lines <$> (getDataFileName "day09-toy-input.txt" >>= readFile)
+    inputLines <- lines <$> (getDataFileName "day09-input.txt" >>= readFile)
+    let seqs = map parseSeq inputLines
+    print seqs
+    print $ sum $ map nextVal seqs
 
 main :: IO ()
 main = day09
