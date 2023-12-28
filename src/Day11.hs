aa86a5e8819346813318f212e98a551a5a7542aa
Oliver Adams <oliver.adams@gmail.com>
2023-12-28 16:28:04 +1000

    Moving to Data.Array...

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,26 +1,39 @@
 module Day11 where
 
 import Paths_aoc2023 (getDataFileName)
-import Data.List (intercalate, elemIndices)
-import Debug.Trace
+import qualified Data.Array as A
+import Data.Array (Array, (!))
+-- import Data.List (intercalate, elemIndices, findIndices)
 
-newtype Universe = Universe { unUniverse :: [[Char]] }
+newtype Universe = Universe { unUniverse :: Array (Int, Int) Char } deriving Show
 
+{-
 instance Show Universe where
     show grid = intercalate "\n" $ unUniverse grid
+-}
 
--- Partial function: raises errors on bad input.
-buildUniverse :: [String] -> Universe
-buildUniverse inputLines
-    | (not . allSameLength) inputLines = error $ "Bad input: " <> show inputLines
-    | otherwise = Universe inputLines
+buildUniverse :: [[Char]] -> Universe
+buildUniverse listOfLists = Universe $ A.listArray ((0, 0), (numRows - 1, numCols - 1)) (concat listOfLists)
+  where
+    numRows = length listOfLists
+    numCols = if numRows > 0 then length (head listOfLists) else 0
+
+getCol :: Int -> Array (Int, Int) a -> [a]
+getCol col arr = [arr ! (i, col) | i <- [l..u]]
+  where
+    ((l, _), (u, _)) = A.bounds arr
+
+findGaps :: Universe -> ([Int], [Int])
+findGaps (Universe universe) = (rowGaps, colGaps)
+  where
+    colGaps = [j | j <- [l..u], all (== '.') (getCol j universe)]
       where
-        allSameLength :: [[a]] -> Bool
-        allSameLength [] = True
-        allSameLength (x:xs) = all (\list -> length list == length x) xs
+        ((_, l), (_, u)) = A.bounds universe
+
 
 -- TODO Explore replacing this with a Data.Array because it's painful to work on columns when
 -- dealing with lists of lists
+{-
 expandUniverse :: Universe -> Universe
 expandUniverse (Universe universe) = Universe (expandRows $ expandCols universe)
   where
@@ -31,12 +44,14 @@ expandUniverse (Universe universe) = Universe (expandRows $ expandCols universe)
         | otherwise = r : expandRows rs
     expandCols :: [[Char]] -> [[Char]]
     expandCols rows
-        | all (== []) rows = trace ("Base case") rows
-        | all (\r -> take 1 r == ['.']) rows = trace ("Empty col") map (\r -> ".." <> r) rest
-        | otherwise = trace ("Otherwise") zipWith (\x y -> take 1 x <> y) rows rest
+        | all (== []) rows = rows
+        | all (\r -> take 1 r == ['.']) rows = map (".." <>) rest
+        | otherwise = zipWith (\x y -> take 1 x <> y) rows rest
           where
             rest = expandCols (map (drop 1) rows)
+-}
 
+{-
 galaxyPositions :: Universe -> [(Int, Int)]
 galaxyPositions (Universe universe) = galaxyPositions' 0 universe 
   where
@@ -46,23 +61,30 @@ galaxyPositions (Universe universe) = galaxyPositions' 0 universe
       where
         js = elemIndices '#' row
         rowCoords = map (\j -> (i, j)) js
+-}
 
 
+-- The idea here is that, given two galaxies x and y, we only want one of (x, y) and (y, x),
+-- and we don't want (x, x) or (y, y), so we use a less-than operator to ensure this.
+{-
 getUniquePairs :: [(Int, Int)] -> [((Int, Int), (Int, Int))]
 getUniquePairs galaxyCoords = filter (uncurry (<)) [(x, y) | x <- galaxyCoords, y <- galaxyCoords]
 
 manhattanDist :: ((Int, Int), (Int, Int)) -> Int
 manhattanDist ((x1, y1), (x2, y2)) = abs (x2 - x1) + abs (y2 - y1)
+-}
 
 day11 :: IO ()
 day11 = do
-    inputLines <- lines <$> (getDataFileName "day11-input.txt" >>= readFile)
+    inputLines <- lines <$> (getDataFileName "day11-toy-input.txt" >>= readFile)
     let universe = buildUniverse inputLines
     print universe
-    putStrLn "----------"
-    print $ expandUniverse universe
-    print $ galaxyPositions $ expandUniverse universe
-    print $ sum $ map manhattanDist $ getUniquePairs $ galaxyPositions $ expandUniverse universe
+    print $ findGaps universe
+    -- putStrLn "----------"
+    -- print $ findGaps universe
+    -- print $ expandUniverse universe
+    -- print $ galaxyPositions $ expandUniverse universe
+    -- print $ sum $ map manhattanDist $ getUniquePairs $ galaxyPositions $ expandUniverse universe
 
 main :: IO ()
 main = day11
